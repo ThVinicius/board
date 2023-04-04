e62ff104684f87feff8cb9fcdc8fbe32ea9f5bf6
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-04 20:40:35 -0300

    feat: paypal provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,25 @@
 import { SessionProvider as NextAuthProvider } from 'next-auth/react'
 import { AppProps } from 'next/app'
 
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
+
 import { Header } from '../components/Header'
 
 import '../styles/global.scss'
 
+const paypalOptions = {
+  'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  currency: 'BRL',
+  intent: 'capture'
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <PayPalScriptProvider options={paypalOptions}>
+        <Header />
+        <Component {...pageProps} />
+      </PayPalScriptProvider>
     </NextAuthProvider>
   )
 }
